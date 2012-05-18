894ca14210c5a570b16993c1637958672b74f95d
Roger Dickey, Jr <roger@do-unlimited.com>
2012-05-18 14:57:56 -0400

    First commit

---
+++ Client/Vidly.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoUnlimited
+{
+    /// <summary>
+    /// Imeplements the methods available via the Vid.ly API http://api.vid.ly/
+    /// </summary>
+    public class Vidly
+    {
+        private string _userId;
+        private string _userKey;
+        private string _notifyUrl;
+
+        public string UserId
+        {
+            get
+            {
+                return this._userId;
+            }
+            set
+            {
+                this._userId = value;
+            }
+        }
+
+        public string UserKey
+        {
+            get
+            {
+                return this._userKey;
+            }
+            set
+            {
+                this._userKey = value;
+            }
+        }
+
+        public string NotifyUrl
+        {
+            get
+            {
+                return this._notifyUrl;
+            }
+            set
+            {
+                this._notifyUrl = value;
+            }
+        }
+
+        public void AddMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddMediaLite()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RequestAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,1 @@
+.NET implementation of the Vid.ly API.
