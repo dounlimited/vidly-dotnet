2b638e0130ca03a2695336bc0086a9b72337b354
Roger Dickey, Jr <roger@do-unlimited.com>
2012-05-18 14:40:29 -0500

    Stubbed out some request & response base classes to handle generic details.

---
+++ Client/VidlyRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoUnlimited
+{
+    public abstract class VidlyRequest
+    {
+        private string _userId;
+        private string _userKey;
+        private string _notify;
+
+        public string UserId
+        {
+            get
+            {
+                return this._userId;
+            }
+            set
+            {
+                this._userId = value;
+            }
+        }
+
+        public string UserKey
+        {
+            get
+            {
+                return this._userKey;
+            }
+            set
+            {
+                this._userKey = value;
+            }
+        }
+
+        public string Notify
+        {
+            get
+            {
+                return this._notify;
+            }
+            set
+            {
+                this._notify = value;
+            }
+        }
+    }
+}
---
+++ Client/VidlyResponse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoUnlimited
+{
+    public abstract class VidlyResponse
+    {
+        private string _message;
+        private decimal _messageCode;
+        private List<object> _success;
+        private List<object> _errors;
+
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                this._message = value;
+            }
+        }
+
+        public decimal MessageCode
+        {
+            get
+            {
+                return this._messageCode;
+            }
+            set
+            {
+                this._messageCode = value;
+            }
+        }
+
+        public List<object> Success
+        {
+            get
+            {
+                return this._success;
+            }
+            set
+            {
+                this._success = value;
+            }
+        }
+
+        public List<object> Errors
+        {
+            get
+            {
+                return this._errors;
+            }
+            set
+            {
+                this._errors = value;
+            }
+        }
+    }
+}
