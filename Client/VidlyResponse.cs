79f402345ebe1baba8ef88a48a5e09cdc67361fc
Roger Dickey, Jr <roger@do-unlimited.com>
2012-05-21 15:46:14 -0400

    The basic VidylResponse is correctly parsing the return data from API calls.

--- Client/VidlyResponse.cs
+++ Client/VidlyResponse.cs
@@ -14,6 +14,12 @@ namespace DoUnlimited.Vidly
         private List<MediaShortLink> _success;
         private List<MediaError> _errors;
 
+        public VidlyResponse()
+        {
+            this._success = new List<MediaShortLink>();
+            this._errors = new List<MediaError>();
+        }
+
         [XmlElement("Message")]
         public string Message
         {
@@ -66,19 +72,42 @@ namespace DoUnlimited.Vidly
             }
         }
 
-        public static VidlyResponse Create(string xml)
-        {
-            StringReader sr = new StringReader(xml);            
-            XmlSerializer serial = new XmlSerializer(typeof(VidlyResponse));
-            XmlReader xr = XmlReader.Create(sr);
-            VidlyResponse response = (VidlyResponse)serial.Deserialize(xr);
-            return response;
-        }
-
         public static VidlyResponse Create(XmlDocument xmlDoc)
         {
             VidlyResponse response = new VidlyResponse();
-            //TODO fill in the VidlyResponse object with xmlDoc contents.
+            response.Message = xmlDoc.DocumentElement.SelectSingleNode("Message").InnerText;
+            response.MessageCode = decimal.Parse(xmlDoc.DocumentElement.SelectSingleNode("MessageCode").InnerText);
+
+            XmlNodeList successNodes = xmlDoc.DocumentElement.SelectNodes("Success/MediaShortLink");
+
+            for (int i = 0; i < successNodes.Count; i++)
+            {
+                XmlNode successNode = successNodes[i];
+                MediaShortLink link = new MediaShortLink();
+
+                link.SourceFile = successNode.SelectSingleNode("SourceFile").InnerText;
+                link.ShortLink = successNode.SelectSingleNode("ShortLink").InnerText;
+                response.Success.Add(link);
+            }
+
+            XmlNodeList errorNodes = xmlDoc.DocumentElement.SelectNodes("Errors/Error");
+
+            for (int i = 0; i < errorNodes.Count; i++)
+            {
+                XmlNode errorNode = errorNodes[i];
+                MediaError error = new MediaError();
+
+                XmlNode sourceFileNode = errorNode.SelectSingleNode("SourceFile");
+                if (sourceFileNode != null)
+                {
+                    error.SourceFile = errorNode.SelectSingleNode("SourceFile").InnerText;
+                }
+                error.ErrorCode = decimal.Parse(errorNode.SelectSingleNode("ErrorCode").InnerText);
+                error.Description = errorNode.SelectSingleNode("Description").InnerText;
+                error.Suggestion = errorNode.SelectSingleNode("Suggestion").InnerText;
+                response.Errors.Add(error);
+            }
+
             return response;
         }
     }
