939fe31829d736a503f2c92e4e48c436026502ef
Roger Dickey, Jr <roger@do-unlimited.com>
2012-05-21 11:31:31 -0400

    Added classes to handle a GetStatus response.

---
+++ Client/MediaStatus.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace DoUnlimited
+{
+    [XmlRoot("Task")]
+    public class MediaStatus
+    {
+        private string _userID;
+        private string _mediaShortLink;
+        private string _sourceFile;
+        private int _batchID;
+        private string _status;
+        private DateTime _created;
+        private DateTime _updated;
+        private string _userEmail;
+
+        public MediaStatus()
+        {
+        }
+
+        [XmlElement("UserID")]
+        public string UserID
+        {
+            get
+            {
+                return this._userID;
+            }
+            set
+            {
+                this._userID = value;
+            }
+        }
+
+        [XmlElement("MediaShortLink")]
+        public string MediaShortLink
+        {
+            get
+            {
+                return this._mediaShortLink;
+            }
+            set
+            {
+                this._mediaShortLink = value;
+            }
+        }
+
+        [XmlElement("SourceFile")]
+        public string SourceFile
+        {
+            get
+            {
+                return this._sourceFile;
+            }
+            set
+            {
+                this._sourceFile = value;
+            }
+        }
+
+        [XmlElement("BatchID")]
+        public int BatchID
+        {
+            get
+            {
+                return this._batchID;
+            }
+            set
+            {
+                this._batchID = value;
+            }
+        }
+
+        [XmlElement("Status")]
+        public string Status
+        {
+            get
+            {
+                return this._status;
+            }
+            set
+            {
+                this._status = value;
+            }
+        }
+
+        [XmlElement("Created")]
+        public DateTime Created
+        {
+            get
+            {
+                return this._created;
+            }
+            set
+            {
+                this._created = value;
+            }
+        }
+
+        [XmlElement("Updated")]
+        public DateTime Updated
+        {
+            get
+            {
+                return this._updated;
+            }
+            set
+            {
+                this._updated = value;
+            }
+        }
+
+        [XmlElement("UserEmail")]
+        public string UserEmail
+        {
+            get
+            {
+                return this._userEmail;
+            }
+            set
+            {
+                this._userEmail = value;
+            }
+        }
+    }
+}
---
+++ Client/StatusMediaResponse.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace DoUnlimited
+{
+    [XmlRoot("Response")]
+    public class StatusMediaResponse
+    {
+        private string _message;
+        private decimal _messageCode;
+        private List<MediaStatus> _mediaStatus;
+        private List<MediaError> _errors;
+
+        public StatusMediaResponse()
+        {
+            this._mediaStatus = new List<MediaStatus>();
+            this._errors = new List<MediaError>();
+        }
+
+        [XmlElement("Message")]
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                this._message = value;
+            }
+        }
+
+        [XmlElement("MessageCode")]
+        public decimal MessageCode
+        {
+            get
+            {
+                return this._messageCode;
+            }
+            set
+            {
+                this._messageCode = value;
+            }
+        }
+
+        [XmlElement("Success")]
+        public List<MediaStatus> MediaStatus
+        {
+            get
+            {
+                return this._mediaStatus;
+            }
+            set
+            {
+                this._mediaStatus = value;
+            }
+        }
+
+        [XmlElement("Errors")]
+        public List<MediaError> Errors
+        {
+            get
+            {
+                return this._errors;
+            }
+            set
+            {
+                this._errors = value;
+            }
+        }
+    }
+}
